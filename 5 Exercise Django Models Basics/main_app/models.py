bfcb7f5b8f84b3d1237a7686f2eaa21d1cfad8d0
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-10-23 12:45:49 +0300

    added Product model

--- 5 Exercise Django Models Basics/main_app/models.py
+++ 5 Exercise Django Models Basics/main_app/models.py
@@ -33,3 +33,17 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True,
     )
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=70,
+    )
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
