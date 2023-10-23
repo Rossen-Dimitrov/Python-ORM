3ea5ee4c9489eab86e452abb61010f82678bff69
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-10-23 13:08:30 +0300

    added Exercise model

--- 5 Exercise Django Models Basics/main_app/models.py
+++ 5 Exercise Django Models Basics/main_app/models.py
@@ -73,3 +73,27 @@ class UserProfile(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True,
     )
+
+
+class Exercise(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+    description = models.TextField()
+    difficulty_level = models.CharField(
+        max_length=20,
+    )
+    duration_minutes = models.PositiveIntegerField()
+    equipment = models.CharField(
+        max_length=90,
+    )
+    video_url = models.URLField(
+        null=True,
+        blank=True,
+    )
+    calories_burned = models.PositiveIntegerField(
+        default=0,
+    )
+    is_favorite = models.BooleanField(
+        default=False,
+    )
