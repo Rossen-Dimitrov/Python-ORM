6e9114e77051cd685a17bd2a0ec569d60797d77d
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-10-23 13:31:27 +0300

    added book model

--- 5 Exercise Django Models Basics/main_app/models.py
+++ 5 Exercise Django Models Basics/main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+GENRE = [
+    ("Fiction", "Fiction"),
+    ("Non-Fiction", "Non-Fiction"),
+    ("Science Fiction", "Science Fiction"),
+    ("Horror", "Horror"),
+]
+
 class Person(models.Model):
     name = models.CharField(
         max_length=30,
@@ -97,3 +104,32 @@ class Exercise(models.Model):
     is_favorite = models.BooleanField(
         default=False,
     )
+
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=30,
+    )
+    author = models.CharField(
+        max_length=100,
+    )
+    genre = models.CharField(
+        max_length=20,
+        choices=GENRE,
+    )
+    publication_date = models.DateField(
+        editable=False,
+        auto_now_add=True
+    )
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+    )
+    is_available = models.BooleanField(
+        default=True,
+    )
+    rating = models.FloatField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
