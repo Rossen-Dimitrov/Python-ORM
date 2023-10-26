45a05e193d93f11ecb1c70252206ab7c4b881cd2
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-10-26 11:29:23 +0300

    10 Order

--- 7 Exercises Migrations and Django Admin/main_app/models.py
+++ 7 Exercises Migrations and Django Admin/main_app/models.py
@@ -57,7 +57,9 @@ class Student(models.Model):
         max_length=50,
     )
 
-    age = models.PositiveIntegerField()
+    age = models.PositiveIntegerField(
+
+    )
     grade = models.CharField(
         max_length=10,
     )
@@ -144,7 +146,8 @@ class Item(models.Model):
 
     price = models.DecimalField(
         max_digits=10,
-        decimal_places=2),
+        decimal_places=2
+    )
 
     quantity = models.PositiveIntegerField(
         default=1,
@@ -169,3 +172,47 @@ class Smartphone(models.Model):
         max_length=20,
         default="empty",
     )
+
+
+class Order(models.Model):
+    ORDER_CHOICES = (
+        ("Pending", "Pending"),
+        ("Completed", "Completed"),
+        ("Cancelled", "Cancelled"),
+    )
+
+    product_name = models.CharField(
+        max_length=30,
+    )
+    customer_name = models.CharField(
+        max_length=100,
+    )
+    order_date = models.DateField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=ORDER_CHOICES,
+    )
+    amount = models.PositiveIntegerField(
+        default=1,
+    )
+    product_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+    )
+    warranty = models.CharField(
+        max_length=100,
+        default="No warranty",
+    )
+    delivery = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer_name}"
