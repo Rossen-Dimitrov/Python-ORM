04cd22799b37a7e499b55e80d2309106346ba564
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-10-30 15:57:40 +0200

    Exercises: Data Operations in Django with Queries Task 4

--- 9 Exercise Data Operations in Django with Queries/caller.py
+++ 9 Exercise Data Operations in Django with Queries/caller.py
@@ -1,12 +1,14 @@
 import os
 import django
 
+from populate_db import populate_model_with_data
+
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Pet, Artifact, Location
+from main_app.models import Pet, Artifact, Location, Car, Task
 
 
 def create_pet(name: str, species: str):
@@ -73,6 +75,7 @@ def get_capitals():
 def delete_first_location():
     Location.objects.first().delete()
 
+
 # location = Location.objects.create(
 #     name='Sofia', region='Sofia Region', population=1329000,
 #     description='The capital of Bulgaria and the largest city in the country', is_capital=False
@@ -91,3 +94,56 @@ def delete_first_location():
 # print(show_all_locations())
 # print(new_capital())
 # print(get_capitals())
+
+def apply_discount():
+    all_cars = Car.objects.all()
+    for car in all_cars:
+        discount = sum(int(x) for x in str(car.year)) / 100
+        car.price_with_discount = float(car.price) - (float(car.price) * discount)
+        car.save()
+
+
+def get_recent_cars():
+    return Car.objects.filter(year__gte=2020).values("model", "price_with_discount")
+
+
+def delete_last_car():
+    Car.objects.last().delete()
+
+
+# Car.objects.create(model='Mercedes C63 AMG', year=2019, color='white', price=120000.00)
+# Car.objects.create(model='Audi Q7 S line', year=2023, color='black', price=183900.00)
+# Car.objects.create(model='Chevrolet Corvette', year=2021, color='dark grey', price=199999.00)
+
+# apply_discount()
+# print(get_recent_cars())
+# price_with_discount()
+
+
+def show_unfinished_tasks():
+    unfinished_tasks = Task.objects.filter(is_finished=False)
+    return '\n'.join(str(x) for x in unfinished_tasks)
+
+
+# print(show_unfinished_tasks())
+
+
+def complete_odd_tasks():
+    tasks = Task.objects.all()
+
+    for task in tasks:
+        if task.id % 2 != 0:
+            task.is_finished = True
+            task.save()
+
+# complete_odd_tasks()
+
+def encode_and_replace(text: str, task_title: str):
+    matching_tasks = Task.objects.filter
+
+# task = Task.objects.create(
+#     title='Sample Task',
+#     description='This is a sample task description',
+#     due_date='2023-10-31',
+#     is_finished=False,
+)
