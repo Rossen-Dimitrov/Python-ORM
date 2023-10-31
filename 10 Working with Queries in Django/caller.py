c19a9e838fc16795702bb047bfd26c61ba576a04
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-10-31 17:51:59 +0200

    Lab: Working with Queries in Django Task 7

--- 10 Working with Queries in Django/caller.py
+++ 10 Working with Queries in Django/caller.py
@@ -158,11 +158,29 @@ def filter_authors_by_birth_year(f_year, s_year):
     return '\n'.join(result)
 
 
-print("Authors born between 1980 and 2000:")
-print(filter_authors_by_birth_year(1980, 2000))
+# print("Authors born between 1980 and 2000:")
+# print(filter_authors_by_birth_year(1980, 2000))
+# print()
+# print("Authors born between 1950 and 1960:")
+# print(filter_authors_by_birth_year(1950, 1960))
+# print()
+# print("Authors born between 2000 and 2010:")
+# print(filter_authors_by_birth_year(2000, 2010))
+
+
+def change_reviewer_name(reviewer_name, new_name):
+    Review.objects.filter(
+        reviewer_name=reviewer_name
+    ).update(reviewer_name=new_name)
+    result = Review.objects.all()
+    return result
+
+
+print("Change Alice Johnson to A.J.:")
+print(change_reviewer_name("Alice Johnson", "A.J."))
 print()
-print("Authors born between 1950 and 1960:")
-print(filter_authors_by_birth_year(1950, 1960))
+print("Change Bob Wilson to Bobby W.:")
+print(change_reviewer_name("Bob Wilson", "Bobby W."))
 print()
-print("Authors born between 2000 and 2010:")
-print(filter_authors_by_birth_year(2000, 2010))
+print("Change A.J. to A. Johnson:")
+print(change_reviewer_name("A.J.", "A. Johnson"))
