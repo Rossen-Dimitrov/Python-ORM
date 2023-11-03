38454396297c42ab87b60ddcef3c521fa635bc7f
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-11-03 17:23:47 +0200

    Lab: Django Models Relations Task 1 - 4

--- 6 Django Models Relations/main_app/models.py
+++ 6 Django Models Relations/main_app/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 
@@ -22,8 +24,87 @@ class Subject(models.Model):
         max_length=10,
     )
     lecturer = models.ForeignKey(
-        to=Lecturer,
+        to="Lecturer",
         on_delete=models.SET_NULL,
         null=True,
         related_name='subjects',
+    )
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Student(models.Model):
+    student_id = models.CharField(
+        max_length=10,
+        primary_key=True,
+    )
+    first_name = models.CharField(
+        max_length=100,
+    )
+    last_name = models.CharField(
+        max_length=100,
+    )
+    birth_date = models.DateField()
+    email = models.EmailField(
+        unique=True,
+    )
+    subjects = models.ManyToManyField(
+        to='Subject',
+        through='StudentEnrollment',
+    )
+
+
+class StudentEnrollment(models.Model):
+    # class Grades(models.TextChoices):
+    #     A = "A",
+    #     B = "B",
+    #     C = "C",
+    #     D = "D",
+    #     F = "F"
+    GRADES = (
+        ('A', "A"),
+        ('B', "B"),
+        ('C', "C"),
+        ('D', "D"),
+        ('F', "F"),
+    )
+
+    student = models.ForeignKey(
+        to='Student',
+        on_delete=models.CASCADE,
+    )
+    subject = models.ForeignKey(
+        to='Subject',
+        on_delete=models.CASCADE,
+    )
+    enrollment_date = models.DateField(
+        default=date.today,
+    )
+    grade = models.CharField(
+        max_length=1,
+        choices=GRADES,
+    )
+
+    # grade = models.CharField(
+    #     max_length=1,
+    #     choices=Grades.choices,
+    # )
+
+class LecturerProfile(models.Model):
+    lecturer = models.OneToOneField(
+        to="Lecturer",
+        on_delete=models.CASCADE,
+    )
+    email = models.EmailField(
+        unique=True,
+    )
+    bio = models.TextField(
+        blank=True,
+        null=True,
+    )
+    office_location = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
     )
