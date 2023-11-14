0b75f212c2441ef3603fa5acb5fbcfbc59d58878
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-11-14 09:25:35 +0200

    Task 1

--- 8 Exercise Advanced Django Model Techniques/caller.py
+++ 8 Exercise Advanced Django Model Techniques/caller.py
@@ -8,7 +8,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 from django.core.exceptions import ValidationError
-from main_app.models import Book, Product, DiscountedProduct, SpiderHero, FlashHero
+from main_app.models import Book, Product, DiscountedProduct, SpiderHero, FlashHero, Document
 
 # from main_app.models import Customer
 #
@@ -83,23 +83,48 @@ from main_app.models import Book, Product, DiscountedProduct, SpiderHero, FlashH
 
 
 # Create instance of SpiderHero
-spiderman = SpiderHero(name="Spider-Man", hero_title="Spider Hero", energy=100)
-# Create instance of FlashHero
-flash = FlashHero(name="The Flash", hero_title="Flash Hero", energy=70)
+# spiderman = SpiderHero(name="Spider-Man", hero_title="Spider Hero", energy=100)
+# # Create instance of FlashHero
+# flash = FlashHero(name="The Flash", hero_title="Flash Hero", energy=70)
+#
+# # Save the instances to the database
+# spiderman.save()
+# flash.save()
+#
+# # Run the special abilities
+# print(spiderman.swing_from_buildings())
+# print(flash.run_at_super_speed())
+# print(spiderman.swing_from_buildings())
+#
+# # Recharge the energy of Spider-Man and The Flash using the mixin method
+# spiderman.recharge_energy(195)
+# flash.recharge_energy(40)
+#
+# # Now you can check the updated energy levels
+# print(f"{spiderman.name} - Energy: {spiderman.energy}")
+# print(f"{flash.name} - Energy: {flash.energy}")
 
-# Save the instances to the database
-spiderman.save()
-flash.save()
 
-# Run the special abilities
-print(spiderman.swing_from_buildings())
-print(flash.run_at_super_speed())
-print(spiderman.swing_from_buildings())
+from django.contrib.postgres.search import SearchVector
 
-# Recharge the energy of Spider-Man and The Flash using the mixin method
-spiderman.recharge_energy(195)
-flash.recharge_energy(40)
+# Create the first 'Document' object with a title and content.
+document1 = Document.objects.create(
+    title="Django Framework 1",
+    content="Django is a high-level Python web framework for building web applications.",
+)
 
-# Now you can check the updated energy levels
-print(f"{spiderman.name} - Energy: {spiderman.energy}")
-print(f"{flash.name} - Energy: {flash.energy}")
+# Create the second 'Document' object with a title and content.
+document2 = Document.objects.create(
+    title="Django Framework 2",
+    content="Django framework provides tools for creating web pages, handling URL routing, and more.",
+)
+
+# Update the 'search_vector' field in the 'Document' model with search vectors.
+Document.objects.update(search_vector=SearchVector('title', 'content'))
+
+# Perform a full-text search for documents containing the words 'django' and 'web framework'.
+results = Document.objects.filter(search_vector='django web framework')
+
+# Print the search results.
+for result in results:
+    print(f"Title: {result.title}")
