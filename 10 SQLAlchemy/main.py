b579018468684c216ea7ead25be541e98c781c04
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-11-16 09:33:53 +0200

    SQL Alchemy

---
+++ 10 SQLAlchemy/main.py
@@ -0,0 +1,8 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import declarative_base
+#
+# DATABASE_URL = 'postgresql+psycopg2://postgres:!QAZxsw2@127.0.0.1/mydatabase'
+# engine = create_engine(DATABASE_URL)
+
+
+from models import User
---
+++ 10 SQLAlchemy/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine
+
+DATABASE_URL = 'postgresql+psycopg2://postgres:!QAZxsw2@127.0.0.1/mydatabase'
+
+engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    email = Column(String)
+
+
+# Base.metadata.create_all(engine)
