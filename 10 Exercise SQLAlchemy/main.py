6ce76ad8577c528993a18b7f4385f2b091f51c98
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-11-17 17:32:38 +0200

    SQL Alchemy task 1 -2

--- 10 Exercise SQLAlchemy/main.py
+++ 10 Exercise SQLAlchemy/main.py
@@ -1,8 +1,64 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from helpers import session_decorator
+from models import Recipe
 from settings import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
-
 Session = sessionmaker(bind=engine)
+
+session = Session()
+
+
+@session_decorator(session)
+def create_recipe(name: str, ingredients: str, instructions: str):
+    new_recipe = Recipe(
+        name=name,
+        ingredients=ingredients,
+        instructions=instructions
+    )
+    session.add(new_recipe)
+
+
+# recipe1 = create_recipe(
+# 'Spaghetti Carbonara', 'Pasta, Eggs, Pancetta, Cheese', 'Cook the pasta, mix it with eggs, pancetta, and cheese')
+# recipe2 = create_recipe(
+# 'Chicken Stir-Fry', 'Chicken, Bell Peppers, Soy Sauce, Vegetables', 'Stir-fry chicken and vegetables with soy sauce')
+# recipe3 = create_recipe(
+# 'Caesar Salad', 'Romaine Lettuce, Croutons, Caesar Dressing', 'Toss lettuce with dressing and top with croutons')
+
+
+# # Query all recipes
+# recipes = session.query(Recipe).all()
+#
+# # Loop through each recipe and print its details
+# for recipe in recipes:
+#     print(f"Recipe name: {recipe.name}")
+
+
+@session_decorator(session)
+def update_recipe_by_name(name: str, new_name: str, new_ingredients: str, new_instructions: str):
+    recipe_to_update = session.query(Recipe).filter_by(name=name).first()
+    if recipe_to_update:
+        recipe_to_update.name = new_name
+        recipe_to_update.ingredients = new_ingredients
+        recipe_to_update.instructions = new_instructions
+
+
+# Update a recipe by name
+update_recipe_by_name(
+    name="Spaghetti Carbonara",
+    new_name="Carbonara Pasta",
+    new_ingredients="Pasta, Eggs, Guanciale, Cheese",
+    new_instructions="Cook the pasta, mix with eggs, guanciale, and cheese"
+)
+
+# Query the updated recipe
+updated_recipe = session.query(Recipe).filter_by(name="Carbonara Pasta").first()
+
+# Print the updated recipe details
+print("Updated Recipe Details:")
+print(f"Name: {updated_recipe.name}")
+print(f"Ingredients: {updated_recipe.ingredients}")
+print(f"Instructions: {updated_recipe.instructions}")
--- 10 Exercise SQLAlchemy/models.py
+++ 10 Exercise SQLAlchemy/models.py
@@ -1,5 +1,25 @@
 from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, ForeignKey
-
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, ForeignKey, Text
 
 Base = declarative_base()
+
+
+class Recipe(Base):
+    __tablename__ = 'recepie'
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    name = Column(
+        String(100),
+        nullable=False,
+    )
+    ingredients = Column(
+        Text,
+        nullable=False,
+    )
+    instructions = Column(
+        Text,
+        nullable=False,
+    )
+
