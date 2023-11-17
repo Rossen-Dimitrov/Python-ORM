4c97062f74306129ea852e447fd16c0d94a9e867
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-11-17 11:48:11 +0200

    SQL Alchemy first Upload

---
+++ 10 Exercise SQLAlchemy/models.py
@@ -0,0 +1,5 @@
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, ForeignKey
+
+
+Base = declarative_base()
--- 10 SQLAlchemy/main.py
+++ 10 SQLAlchemy/main.py
@@ -3,17 +3,17 @@ from sqlalchemy.orm import sessionmaker
 
 from settings import DATABASE_URL
 
-from models import User
+from models import User, Order
 
 engine = create_engine(DATABASE_URL)
 
 Session = sessionmaker(bind=engine)
 
 # create user
-with Session() as session:
-    new_user = User(username='john_doe', email='john@example.com')
-    session.add(new_user)
-    session.commit()
+# with Session() as session:
+#     new_user = User(username='john_doe', email='john@example.com')
+#     session.add(new_user)
+#     session.commit()
 
 
 # retrieve all users
@@ -45,3 +45,31 @@ with Session() as session:
 #         print("User deleted successfully")
 #     else:
 #         print("User not found")
+
+# with Session() as session:
+#
+#     new_user = User(username='john_doe', email='john@example.com')
+#     new_user2 = User(username='doe_john', email='doe@example.com')
+#     session.add(new_user)
+#     session.add(new_user2)
+#     session.commit()
+#     print(f'import completed.')
+
+# def populate_order_table():
+#     with Session() as session:
+#         session.add_all((Order(user_id=1), Order(user_id=2)))
+#         session.commit()
+#
+# populate_order_table()
+
+# def relationship_query():
+#     with Session() as session:
+#         orders = session.query(Order).order_by(Order.user_id.desc()).all()
+#         if not orders:
+#             print("No orders yet.")
+#             return
+#         for order in orders:
+#             user = order.user
+#             print(f'Order number {order.id}, Is completed: {order.is_completed}, Username: {user.username}')
+#
+# relationship_query()
