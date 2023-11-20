2f1189d38e2a754874bbfeee774cd04f22ecfec9
Rosen Dimitrov <rosen.dimitrov@test.com>
2023-11-20 11:37:27 +0200

    Added more solution

--- 10 Exercise SQLAlchemy/models.py
+++ 10 Exercise SQLAlchemy/models.py
@@ -1,25 +1,52 @@
-from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, ForeignKey, Text
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class Recipe(Base):
-    __tablename__ = 'recepie'
+    __tablename__ = 'recipes'
+
     id = Column(
         Integer,
         primary_key=True,
     )
+
     name = Column(
         String(100),
         nullable=False,
     )
+
     ingredients = Column(
         Text,
         nullable=False,
     )
+
     instructions = Column(
         Text,
         nullable=False,
     )
 
+    chef_id = Column(
+        Integer,
+        ForeignKey('chefs.id')
+    )
+
+    chef = relationship("Chef", back_populates="recipes")
+
+
+class Chef(Base):
+    __tablename__ = 'chefs'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+
+    name = Column(
+        String(100),
+        nullable=False,
+    )
+
+    recipes = relationship("Recipe", back_populates="chef")
